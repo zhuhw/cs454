15632a2f70e8e502634fdbdbdae9fde2357ef082
Henry <hongwenzhu@hotmail.com>
2014-06-19 14:41:33 -0400

    3

--- stringClient.cc
+++ stringClient.cc
@@ -1,21 +1,85 @@
 #include <iostream>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <cstring>
+#include <sstream>
+#include <cstdlib>
+#include <unistd.h>
+
+#include "socketIO.cc"
 
 using namespace std;
 
-int main(int argc, char *argv[]) {
-	if (argc != 1) {
-		cout << "argc should be 1\n";
-		return 0;
-	}
+int main (int argc, char *argv[]) {
+    // exit if we don't have exactly 1 argument
+    if (argc != 1) {
+        cerr << "argc should be 1" << endl;
+        return 0;
+    }
 
-	char *serverAddress = getenv("SERVER_ADDRESS");
-	if (serverAddress == NULL) {
-		cout << "$SERVER_ADDRESS is not set\n";
-		return 0;
-	}
-	char *serverPort = getenv("SERVER_PORT");
-	if (serverPort == NULL) {
-		cout << "$SERVER_PORT is not set\n";
-		return 0;
-	}
+    struct hostent *hp;
+    struct sockaddr_in siServer;
+
+    int clientSocket = 0;
+    int maxSocketFd = 1;
+    char buf[BUFLEN];
+    fd_set readfds;
+
+    char *serverAddress = getenv("SERVER_ADDRESS");
+    if (serverAddress == NULL) {
+        cout << "$SERVER_ADDRESS is not set\n";
+        return 0;
+    }
+    char *serverPort = getenv("SERVER_PORT");
+    if (serverPort == NULL) {
+        cout << "$SERVER_PORT is not set\n";
+        return 0;
+    }
+
+    if ((clientSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
+        cerr << "Cannot create socket" << endl;
+        return 0;
+    }
+
+    memset((char *)&siServer, 0, sizeof(siServer));
+    siServer.sin_family = AF_INET;
+    siServer.sin_port = htons(atoi(serverPort));
+
+    hp = gethostbyname(serverAddress);
+    if (!hp) {
+        cout << "could not resolve hostname!" << endl;
+        return 0;
+    }
+
+    memcpy((void *)&siServer.sin_addr, hp->h_addr_list[0], hp->h_length);
+
+    if (connect(clientSocket, (struct sockaddr *)&siServer, sizeof(siServer)) < 0) {
+        cerr << "Connection Failed" << endl;
+        return 0;
+    }
+
+
+    // get user input
+    string line;
+    while(getline(cin, line)) {
+        if (line.length() == 0) {
+            continue;
+        }
+
+    	if (write(clientSocket, line.c_str(), line.length()) < 0) {
+			cerr << "write failed" << endl;
+			return 0;
+		}
+
+        char buf[BUFLEN];
+        int n = 0;
+
+        if ((n = read(clientSocket, buf, BUFLEN)) > 0) {
+            buf[n] = 0;
+            cout << "Server: " << buf << endl;
+        }
+    }
+
+    close(clientSocket);
+    return 0;
 }
--- stringServer.cc
+++ stringServer.cc
@@ -10,7 +10,7 @@
 #include <sstream>
 #include <vector>
 
-#define BUFLEN 2048
+#include "socketIO.cc"
 
 using namespace std;
 
@@ -28,8 +28,6 @@ int main(int argc, char *argv[]) {
     struct sockaddr_in siServer;
     fd_set readfds;
 
-
-
     if ((serverSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
         cerr << "Cannot create master socket for listen" << endl;
         return 0;
@@ -38,7 +36,6 @@ int main(int argc, char *argv[]) {
     // set server info
     memset((char *)&siServer, 0, sizeof(siServer));
     siServer.sin_family = AF_INET;
-    siServer.sin_port = htons(0);
     siServer.sin_addr.s_addr = htonl(INADDR_ANY);
 
     if ((bind(serverSocket, (const struct sockaddr*)&siServer, sizeof(struct sockaddr_in))) < 0) {
@@ -46,118 +43,93 @@ int main(int argc, char *argv[]) {
         return 0;
     }
 
-	char hostName[128];
-	if (gethostname(hostName, sizeof(hostName)) < 0) {
-		cerr << "Cannot get host name" << endl;
-		return 0;
-	}
+    char hostName[128];
+    if (gethostname(hostName, sizeof(hostName)) < 0) {
+        cerr << "Cannot get host name" << endl;
+        return 0;
+    }
 
-	struct sockaddr_in sin;
-	socklen_t len = sizeof(sin);
-	if (getsockname(serverSocket, (struct sockaddr *)&sin, &len) < 0) {
-	    cerr << "Cannot get socket name" << endl;
-		return 0;
-	}
+    struct sockaddr_in sin;
+    socklen_t len = sizeof(sin);
+    if (getsockname(serverSocket, (struct sockaddr *)&sin, &len) < 0) {
+        cerr << "Cannot get socket name" << endl;
+        return 0;
+    }
 
     cout << "SERVER_ADDRESS " << hostName << endl
-		 << "SERVER_PORT " << ntohs(sin.sin_port) << endl;
+         << "SERVER_PORT " << ntohs(sin.sin_port) << endl;
 
-    if (listen(serverSocket, 10) < 0) {
+    if (listen(serverSocket, 5) < 0) {
         cerr << "lisen failed" << endl;
         return 0;
     }
 
-    try {
-        // Wait for message from client
-        for (;;) {
-            // clear read socket sets
-            FD_ZERO(&readfds);
+    // Wait for message from client
+    for (;;) {
+        // clear read socket sets
+        FD_ZERO(&readfds);
 
-            // add server socket to the read set explicity
-            FD_SET(serverSocket, &readfds);
-            maxSocketFd = serverSocket + 1;
+        // add server socket to the read set explicity
+        FD_SET(serverSocket, &readfds);
+        maxSocketFd = serverSocket + 1;
 
-            // add all established connection sockets to read set
-            for (vector<int>::iterator it = clientSockets.begin();
-                 it != clientSockets.end(); ++it) {
-                if (*it > 0) {
-                    FD_SET( *it, &readfds);
-                }
-
-                if (*it >= maxSocketFd) {
-                    maxSocketFd = *it + 1;
-                }
+        // add all established connection sockets to read set
+        for (vector<int>::iterator it = clientSockets.begin(); it != clientSockets.end(); ++it) {
+            if (*it > 0) {
+                FD_SET(*it, &readfds);
             }
 
-            // some error on select(), maybe need to dump errono
-            if (select(maxSocketFd, &readfds, NULL, NULL, NULL) < 0) {
-                cerr << "Select error" << endl;
+            if (*it >= maxSocketFd) {
+                maxSocketFd = *it + 1;
             }
+        }
 
-            if (FD_ISSET(serverSocket, &readfds)) {
-                // New connection
-                int fd = accept(serverSocket, (struct sockaddr*)NULL, NULL);
-                if (fd < 0) {
-                    cerr << "Accept error" << endl;
-                    return 0;
-                }
-                clientSockets.push_back(fd);
+        // some error on select(), maybe need to dump errono
+        if (select(maxSocketFd, &readfds, NULL, NULL, NULL) < 0) {
+            cerr << "Select error" << endl;
+        }
+
+        if (FD_ISSET(serverSocket, &readfds)) {
+            // New connection
+            int fd = accept(serverSocket, (struct sockaddr*)NULL, NULL);
+            if (fd < 0) {
+                cerr << "Accept error" << endl;
+                return 0;
             }
+            clientSockets.push_back(fd);
+        }
 
-            for (vector<int>::iterator it = clientSockets.begin();
-                 it != clientSockets.end(); ++it) {
-                if (FD_ISSET(*it, &readfds)) {
-                    // Handle incomming message
-                    char buf[BUFLEN];
-                    int n = 0;
+        for (vector<int>::iterator it = clientSockets.begin(); it != clientSockets.end(); ++it) {
+            if (FD_ISSET(*it, &readfds)) {
+                // Handle incomming message
+                char buf[BUFLEN];
+                int n = 0;
 
-                    if ((n = read(*it, buf, BUFLEN)) > 0) {
-                        buf[n] = 0;
-                        string response;
-                        stringstream ss(buf);
-                        string token;
-                        ss >> token;
-                        if (token == "GET") {
+                if ((n = read(*it, buf, BUFLEN)) > 0) {
+                    buf[n] = 0;
+                    cout << buf << endl;
 
-                        } else if (token == "STOP") {
-                            throw exception();
-                        } else if (token == "STOP_SESSION") {
-                            // remove the socket from the vector and then
-                            // close the socket.
-                            shutdown(*it, 2 /* Further sends and
-                                             * receives disallowed */);
-                            close(*it);
-                            clientSockets.erase(it);
-                            it--;
-                        }
-
-                        if (write(*it, response.c_str(), response.length())
-                            < 0) {
-                            cerr << "write failed" << endl;
-                            return 0;
+                    buf[0] = toupper(buf[0]);
+                    for (int i = 1; i < n; i++) {
+                        if (buf[i - 1] == ' ') {
+                            buf[i] = toupper(buf[i]);
+                        } else {
+                            buf[i] = tolower(buf[i]);
                         }
                     }
 
-                    if (n < 0) {
-                        cout << "Read Error" << endl;
-                        // consider whether we need to handle such
-                        // situaltion gracefully, for example, close the
-                        // socket connection.
+                    string response = buf;
+                    if (write(*it, response.c_str(), response.length()) < 0) {
+                        cerr << "write failed" << endl;
                         return 0;
                     }
                 }
+
+                // if (n < 0) {
+                //     cout << "Read Error" << endl;
+                //     return 0;
+                // }
             }
         }
-    } catch(exception &e) {
-        // Close all pending session
-        for (vector<int>::iterator it = clientSockets.begin();
-             it != clientSockets.end(); ++it) {
-            shutdown(*it, 2 /* Further sends and receives
-                             * disallowed */);
-            close(*it);
-        }
-        // Close server socket
-        shutdown(serverSocket, 2);
-        close(serverSocket);
     }
 }
