6de787a655a13d6eed503ddc2a951876f0483188
Ethan Song <skyey0417@gmail.com>
2014-07-05 00:06:18 -0400

    makefile and starter files

---
+++ a3/rpc_server.cc
@@ -0,0 +1,18 @@
+#include "rpc.h"
+#include <iostream>
+
+using namespace std;
+
+
+int rpcInit() {
+    cout << "123" <<endl;
+    return 0;
+}
+
+int rpcRegister(char* name, int* argTypes, skeleton f) {
+    return 0;
+}
+
+int rpcExecute() {
+    return 0;
+}
